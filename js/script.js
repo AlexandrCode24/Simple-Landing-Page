865feb224f40a0d5ef60f59bcc2db971865332ea
Alexander <alexander.campbell.dev@outlook.com>
2025-09-15 23:39:39 -0400

    Single Page Website

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    let fName = document.getElementById("fname").value.trim();
+    let lName = document.getElementById("lname").value.trim();
+    let email = document.getElementById("email").value.trim();
+
+    if (fName && lName) {
+        //print error message 
+    }
+
+    if (email) {
+
+    }
+}
